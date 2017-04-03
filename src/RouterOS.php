a2f7afe19bc79b5c93c451a32b778302633a8d2c
am05mhz <am05mhz@live.com>
2017-04-03 15:17:11 +0700

    first commit

---
+++ src/RouterOS.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace am05mhz;
+
+class RouterOS{
+    private $debug     = false;
+    private $connected = false;
+    private $port      = 8728;
+    private $ssl       = false;
+    private $timeout   = 3;
+    private $attempts  = 5;
+    private $delay     = 3;
+
+    private $socket;
+    private $error_no;
+    private $error_str;
+	
+    public function __destruct()
+    {
+        $this->disconnect();
+    }
+
+    private function isIterable($var)
+    {
+        return $var !== null
+                && (is_array($var)
+                || $var instanceof Traversable
+                || $var instanceof Iterator
+                || $var instanceof IteratorAggregate
+                );
+    }
+	
+    private function debug($text)
+    {
+        if ($this->debug) {
+            echo $text . "\n";
+        }
+    }
+
+    private function encodeLength($length)
+    {
+        if ($length < 0x80) {
+            $length = chr($length);
+        } elseif ($length < 0x4000) {
+            $length |= 0x8000;
+            $length = chr(($length >> 8) & 0xFF) . chr($length & 0xFF);
+        } elseif ($length < 0x200000) {
+            $length |= 0xC00000;
+            $length = chr(($length >> 16) & 0xFF) . chr(($length >> 8) & 0xFF) . chr($length & 0xFF);
+        } elseif ($length < 0x10000000) {
+            $length |= 0xE0000000;
+            $length = chr(($length >> 24) & 0xFF) . chr(($length >> 16) & 0xFF) . chr(($length >> 8) & 0xFF) . chr($length & 0xFF);
+        } elseif ($length >= 0x10000000) {
+            $length = chr(0xF0) . chr(($length >> 24) & 0xFF) . chr(($length >> 16) & 0xFF) . chr(($length >> 8) & 0xFF) . chr($length & 0xFF);
+        }
+
+        return $length;
+    }
+
+    public function connect($ip, $login, $password)
+    {
+        for ($ATTEMPT = 1; $ATTEMPT <= $this->attempts; $ATTEMPT++) {
+            $this->connected = false;
+            $PROTOCOL = ($this->ssl ? 'ssl://' : '' );
+            $context = stream_context_create([
+					'ssl' => ['ciphers' => 'ADH:ALL', 'verify_peer' => false, 'verify_peer_name' => false]
+				]);
+            $this->debug('Connection attempt #' . $ATTEMPT . ' to ' . $PROTOCOL . $ip . ':' . $this->port . '...');
+            $this->socket = @stream_socket_client($PROTOCOL . $ip.':'. $this->port, $this->error_no, $this->error_str, $this->timeout, STREAM_CLIENT_CONNECT, $context);
+            if ($this->socket) {
+                socket_set_timeout($this->socket, $this->timeout);
+                $this->write('/login');
+                $RESPONSE = $this->read(false);
+                if (isset($RESPONSE[0]) && $RESPONSE[0] == '!done') {
+                    $MATCHES = array();
+                    if (preg_match_all('/[^=]+/i', $RESPONSE[1], $MATCHES)) {
+                        if ($MATCHES[0][0] == 'ret' && strlen($MATCHES[0][1]) == 32) {
+                            $this->write('/login', false);
+                            $this->write('=name=' . $login, false);
+                            $this->write('=response=00' . md5(chr(0) . $password . pack('H*', $MATCHES[0][1])));
+                            $RESPONSE = $this->read(false);
+                            if (isset($RESPONSE[0]) && $RESPONSE[0] == '!done') {
+                                $this->connected = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                fclose($this->socket);
+            }
+            sleep($this->delay);
+        }
+
+        if ($this->connected) {
+            $this->debug('Connected...');
+        } else {
+            $this->debug('Error...');
+        }
+        return $this->connected;
+    }
+
+    public function disconnect()
+    {
+        // let's make sure this socket is still valid.  it may have been closed by something else
+        if( is_resource($this->socket) ) {
+            fclose($this->socket);
+        }
+        $this->connected = false;
+        $this->debug('Disconnected...');
+    }
+
+    private function parseResponse($response)
+    {
+        if (is_array($response)) {
+            $PARSED      = array();
+            $CURRENT     = null;
+            $singlevalue = null;
+            foreach ($response as $x) {
+                if (in_array($x, array('!fatal','!re','!trap'))) {
+                    if ($x == '!re') {
+                        $CURRENT =& $PARSED[];
+                    } else {
+                        $CURRENT =& $PARSED[$x][];
+                    }
+                } elseif ($x != '!done') {
+                    $MATCHES = array();
+                    if (preg_match_all('/[^=]+/i', $x, $MATCHES)) {
+                        if ($MATCHES[0][0] == 'ret') {
+                            $singlevalue = $MATCHES[0][1];
+                        }
+                        $CURRENT[$MATCHES[0][0]] = (isset($MATCHES[0][1]) ? $MATCHES[0][1] : '');
+                    }
+                }
+            }
+
+            if (empty($PARSED) && !is_null($singlevalue)) {
+                $PARSED = $singlevalue;
+            }
+
+            return $PARSED;
+        } else {
+            return array();
+        }
+    }
+
+    private function arrayChangeKeyName($array)
+    {
+        if (is_array($array)) {
+            foreach ($array as $k => $v) {
+                $tmp = str_replace(['-', '/'], '_', $k);
+				$array_new[$tmp] = $v;
+            }
+            return $array_new;
+        } else {
+            return $array;
+        }
+    }
+
+    private function read($parse = true)
+    {
+        $RESPONSE     = array();
+        $receiveddone = false;
+        while (true) {
+            // Read the first byte of input which gives us some or all of the length
+            // of the remaining reply.
+            $BYTE   = ord(fread($this->socket, 1));
+            $LENGTH = 0;
+            // If the first bit is set then we need to remove the first four bits, shift left 8
+            // and then read another byte in.
+            // We repeat this for the second and third bits.
+            // If the fourth bit is set, we need to remove anything left in the first byte
+            // and then read in yet another byte.
+            if ($BYTE & 128) {
+                if (($BYTE & 192) == 128) {
+                    $LENGTH = (($BYTE & 63) << 8) + ord(fread($this->socket, 1));
+                } else {
+                    if (($BYTE & 224) == 192) {
+                        $LENGTH = (($BYTE & 31) << 8) + ord(fread($this->socket, 1));
+                        $LENGTH = ($LENGTH << 8) + ord(fread($this->socket, 1));
+                    } else {
+                        if (($BYTE & 240) == 224) {
+                            $LENGTH = (($BYTE & 15) << 8) + ord(fread($this->socket, 1));
+                            $LENGTH = ($LENGTH << 8) + ord(fread($this->socket, 1));
+                            $LENGTH = ($LENGTH << 8) + ord(fread($this->socket, 1));
+                        } else {
+                            $LENGTH = ord(fread($this->socket, 1));
+                            $LENGTH = ($LENGTH << 8) + ord(fread($this->socket, 1));
+                            $LENGTH = ($LENGTH << 8) + ord(fread($this->socket, 1));
+                            $LENGTH = ($LENGTH << 8) + ord(fread($this->socket, 1));
+                        }
+                    }
+                }
+            } else {
+                $LENGTH = $BYTE;
+            }
+
+            $_ = '';
+
+            // If we have got more characters to read, read them in.
+            if ($LENGTH > 0) {
+                $_      = '';
+                $retlen = 0;
+                while ($retlen < $LENGTH) {
+                    $toread = $LENGTH - $retlen;
+                    $_ .= fread($this->socket, $toread);
+                    $retlen = strlen($_);
+                }
+                $RESPONSE[] = $_;
+                $this->debug('>>> [' . $retlen . '/' . $LENGTH . '] bytes read.');
+            }
+
+            // If we get a !done, make a note of it.
+            if ($_ == '!done') {
+                $receiveddone = true;
+            }
+
+            $STATUS = socket_get_status($this->socket);
+            if ($LENGTH > 0) {
+                $this->debug('>>> [' . $LENGTH . ', ' . $STATUS['unread_bytes'] . ']' . $_);
+            }
+
+            if ((!$this->connected && !$STATUS['unread_bytes']) || ($this->connected && !$STATUS['unread_bytes'] && $receiveddone)) {
+                break;
+            }
+        }
+
+        if ($parse) {
+            $RESPONSE = $this->parseResponse($RESPONSE);
+        }
+
+        return $RESPONSE;
+    }
+
+    private function write($command, $param2 = true)
+    {
+        if ($command) {
+            $data = explode("\n", $command);
+            foreach ($data as $com) {
+                $com = trim($com);
+                fwrite($this->socket, $this->encodeLength(strlen($com)) . $com);
+                $this->debug('<<< [' . strlen($com) . '] ' . $com);
+            }
+
+            if (gettype($param2) == 'integer') {
+                fwrite($this->socket, $this->encodeLength(strlen('.tag=' . $param2)) . '.tag=' . $param2 . chr(0));
+                $this->debug('<<< [' . strlen('.tag=' . $param2) . '] .tag=' . $param2);
+            } elseif (gettype($param2) == 'boolean') {
+                fwrite($this->socket, ($param2 ? chr(0) : ''));
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function comm($com, $arr = array())
+    {
+        $count = count($arr);
+        $this->write($com, !$arr);
+        $i = 0;
+        if ($this->isIterable($arr)) {
+            foreach ($arr as $k => $v) {
+                switch ($k[0]) {
+                    case '?':
+                        $el = "$k=$v";
+                        break;
+                    case '~':
+                        $el = "$k~$v";
+                        break;
+                    default:
+                        $el = "=$k=$v";
+                        break;
+                }
+
+                $last = ($i++ == $count - 1);
+                $this->write($el, $last);
+            }
+        }
+
+        return $this->read();
+    }
+}
